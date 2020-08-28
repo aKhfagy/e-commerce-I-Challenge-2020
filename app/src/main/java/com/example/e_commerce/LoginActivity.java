66ebf2ab38949a70a675a7644d371d947b7e5932
Sara <saranor@gmail.com>
2020-08-28 21:33:11 +0200

    add maps and menu

--- app/src/main/java/com/example/e_commerce/LoginActivity.java
+++ app/src/main/java/com/example/e_commerce/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -11,6 +13,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.e_commerce.login.LoginDbHelper;
@@ -100,7 +103,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
             case R.id.forget_link:
                 startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
-                Toast.makeText(LoginActivity.this, "fff", Toast.LENGTH_LONG).show();
+               // Toast.makeText(LoginActivity.this, "fff", Toast.LENGTH_LONG).show();
                 break;
 
             case R.id.register_link:
@@ -108,5 +111,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 break;
 
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile_link:
+               // startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                return true;
+            case R.id.location_link:
+                startActivity(new Intent(LoginActivity.this, MapsActivity.class));
+                finish();
+                return true;
+            case R.id.logout_link:
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(User.REMEMBER_ME, false);
+                editor.apply();
+                return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/e_commerce/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.e_commerce;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,11 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
         if (sharedPreferences.getBoolean(User.REMEMBER_ME, false)) {
             startActivity(new Intent(MainActivity.this, ChooseProductActivity.class));
-            finish();
+
         }
         else {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
+
         }
     }
+
 }
