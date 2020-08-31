56fc1c3e09a54e23729bb02e2eb4b15eb82f00c1
pierre <pieer44@gmail.com>
2020-08-31 04:08:04 +0200

    Added Mail confirmation

--- app/src/main/java/com/example/e_commerce/ShoppingCartActivity.java
+++ app/src/main/java/com/example/e_commerce/ShoppingCartActivity.java
@@ -1,7 +1,11 @@
 package com.example.e_commerce;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,21 +13,32 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.e_commerce.login.Constants;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCartActivity extends AppCompatActivity {
     static ArrayList<Item> itemList = new ArrayList<>();
     public static Double Total = 0.0;
-    public static TextView total;
+    public static Button total;
+    private SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
         itemList = Product.chosenItems;
+        sharedPreferences = ShoppingCartActivity.this.getSharedPreferences(Constants.PREFERENCE_NAME, Context.MODE_PRIVATE);
         Total = countTotal();
         total = findViewById(R.id.total);
+        total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+            }
+        });
         total.setText("Total : " + Total);
         System.out.println(itemList.get(0).getName());
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
@@ -42,5 +57,18 @@ public class ShoppingCartActivity extends AppCompatActivity {
         }
         return sum;
     }
+
+    void sendMail(){
+        String mail = sharedPreferences.getString(Constants.UserTable.EMAIL,"");
+        String message = "You Ordered: \n\n";
+        for (int i = 0;i<itemList.size();i++){
+            message += itemList.get(i).getQuantity() + " " + itemList.get(i).getName() + " : " + itemList.get(i).getCost() + "$" + "\n";
+        }
+        message += "\nYou will pay " + countTotal() + "$";
+        String subject = "Confirmation Mail";
+        MailAPI mailAPI = new MailAPI(this,mail,subject,message);
+        mailAPI.execute();
+    }
+
 }
 
---
+++ app/src/main/java/com/example/e_commerce/Utils.java
@@ -0,0 +1,7 @@
+package com.example.e_commerce;
+
+public class Utils {
+    public static final String email = "ecommerceappichallenge@gmail.com";
+    public static final String password = "Ecommerce55";
+
+}
